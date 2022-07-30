5002edd713485b8de9f338dfc301492809fd5834
shifthash <venkateshreddy.eruva@gmail.com>
2022-07-30 19:04:32 +0200

    jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,48 @@
-pipeline{
+pipeline {
     agent any
 
     environment {
+        registry = ""
+        registryCredential = 'docker_akshay'
+        dockerImage = ''
 
+    }
+
+
+    stages {
+        stage('Cloning Git') {
+            steps {
+                git branch: 'master', url: 'https://github.com/ShiftHash/javaeeapp.git'
+            }
+        }
+        stage('Building  image') {
+            steps {
+                script {
+                    dockerImage = docker.build registry + ":$BUILD_NUMBER"
+                }
+            }
+        }
+        stage('Deploy image') {
+            steps {
+                script {
+                    docker.withRegistry( '', registryCredential ) {
+                        dockerImage.push("$BUILD_NUMBER")
+                        dockerImage.push('latest')
+                    }
+                }
+            }
+        }
+        stage('Docker Run') {
+            steps{
+                script {
+                    dockerImage.run("-p 8000:8000 --name api_hello")
+                }
+            }
+        }
+        stage('Cleaning up'){
+            steps {
+                sh "docker rmi $registry:$BUILD_NUMBER"
+            }
+        }
     }
 }
