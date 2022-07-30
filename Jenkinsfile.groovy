dfb6f0f0bf233e0123eeb230853d403d6a5deb62
shifthash <venkateshreddy.eruva@gmail.com>
2022-07-30 19:26:08 +0200

    jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,12 +1,6 @@
 pipeline {
     agent any
 
-    environment {
-        registry = "javaeeapp"
-        registryCredential = 'docker_ev'
-        dockerImage = ''
-
-
     }
 
     stages {
@@ -20,34 +14,6 @@ pipeline {
                 sh "mvn clean install"
             }
         }
-        stage('Building  image') {
-            steps {
-                script {
-                    dockerImage = docker.build registry + ":$BUILD_NUMBER"
-                }
-            }
-        }
-        stage('Deploy image') {
-            steps {
-                script {
-                    docker.withRegistry( '', registryCredential ) {
-                        dockerImage.push("$BUILD_NUMBER")
-                        dockerImage.push('latest')
-                    }
-                }
-            }
-        }
-        stage('Docker Run') {
-            steps{
-                script {
-                    dockerImage.run("-p 8000:8000 --name api_hello")
-                }
-            }
-        }
-        stage('Cleaning up'){
-            steps {
-                sh "docker rmi $registry:$BUILD_NUMBER"
-            }
-        }
+
     }
 }
